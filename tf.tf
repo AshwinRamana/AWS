c29b89c660775449fca90ec55f66cd297bd1c663
AshwinRamana <100801959+AshwinRamana@users.noreply.github.com>
2022-06-22 20:12:55 -0400

    Add files via upload

---
+++ tf.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ca-central-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-00f881f027a6d74a0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ashwin"
+  }
+}
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ca-central-1a", "ca-central-1b", "ca-central-1d"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+resource "aws_s3_bucket" "b" {
+  bucket = "ashbucket477"
+
+  tags = {
+    Name        = "AShwsww"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
